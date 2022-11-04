16ca7d97d3ad535486c5b8daf16941c11fb6f205
Svitac6 <fossierlucie@gmail.com>
2022-11-04 12:38:41 +0100

    updtae class Ville

--- src/projet/ville.java
+++ src/projet/ville.java
@@ -1,10 +1,33 @@
-package carte;
+package projetJava;
 
-public class ville {
+import java.util.List;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Ville {
+	private String name;
+	private List<Arete> arete;
+	
+	public Ville(String name){
+		this.name = name;
 	}
-
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getArete() {
+		return this.arete;
+	}
+	
+	
+	
+	
+	public void addAretes(Ville ville, int distance ) {
+		
+	}
+	
+   
 }
