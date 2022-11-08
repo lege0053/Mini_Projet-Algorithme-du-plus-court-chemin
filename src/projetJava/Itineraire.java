13f637103b760d42021ffd510f0a1d0599b6707c
lege0053 <marine.legendre@etudiant.univ-reims.fr>
2022-11-08 10:33:07 +0100

    wip Itineraire

--- src/projetJava/Itineraire.java
+++ src/projetJava/Itineraire.java
@@ -1,5 +1,45 @@
 package projetJava;
 
 public class Itineraire {
+	
+	private Carte carte;
+	private Ville source;
+	private Ville destination;
+	
+	public Itineraire(Carte carte, Ville source, Ville destination) {
+		this.carte = carte;
+		this.source = source;
+		this.destination = destination;
+	}
+	
+	public Carte getCarte() {
+		return carte;
+	}
+	
+	public void setCarte(Carte carte) {
+		this.carte = carte;
+	}
+	
+	public Ville getSource() {
+		return source;
+	}
+	
+	public void setSource(Ville source) {
+		this.source = source;
+	}
+	
+	public Ville getDestination() {
+		return destination;
+	}
+	
+	public void setDestination(Ville destination) {
+		this.destination = destination;
+	}
+	
+	@Override
+	public String toString() {
+		return "Itineraire [carte=" + carte + ", source=" + source + ", destination=" + destination + "]";
+	}
 
+	
 }
